6f4b0a53bc529b84b1e6fa448861b26e20859768
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-21 12:43:00 +0530

    feat: add map component with random coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { MapComponent } from './components';
 
 function App() {
   return (
     <div className="App">
-      
+      <MapComponent />
     </div>
   );
 }
