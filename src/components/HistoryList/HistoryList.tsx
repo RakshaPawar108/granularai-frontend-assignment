67ec053aff0cb314152cd551ae6ac56ee421fc92
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-22 12:35:58 +0530

    feat: add link share button

--- src/components/HistoryList/HistoryList.tsx
+++ src/components/HistoryList/HistoryList.tsx
@@ -1,9 +1,24 @@
+import "./HistoryList.css";
+
 export const HistoryList = () => {
   return (
-    <div className="list-popup-card">
-      <div className="popup-heading">History</div>
-      <div className="popup-list-items">
-        <li className="popup-list-item">Item One</li>
+    <div className="list-popup-bg">
+      <div className="list-popup-card">
+        <div className="popup-heading">History</div>
+        <div className="popup-list-items">
+          <li className="popup-list-item">
+            Item One <div className="link-share-button">Share</div>
+          </li>
+          <li className="popup-list-item">
+            Item Two <div className="link-share-button">Share</div>
+          </li>
+          <li className="popup-list-item">
+            Item Three <div className="link-share-button">Share</div>
+          </li>
+          <li className="popup-list-item">
+            Item Four <div className="link-share-button">Share</div>
+          </li>
+        </div>
       </div>
     </div>
   );
