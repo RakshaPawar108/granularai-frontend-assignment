d4c374aec2315692da410ceed60bf1c975fef126
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-22 11:36:58 +0530

    style: add styling to option list

--- src/components/OptionList/OptionList.tsx
+++ src/components/OptionList/OptionList.tsx
@@ -1,6 +1,10 @@
+import './OptionList.css'
+
 export const OptionList = () => {
   return (
     <div className="option-list">
+      <div className="option-item">Option Name</div>
+      <div className="option-item">Option Name</div>
       <div className="option-item">Option Name</div>
     </div>
   );
