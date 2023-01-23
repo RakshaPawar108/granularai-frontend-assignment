0d6868ee5b8da015b5110c048127686d63d022d2
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-23 11:12:05 +0530

    feat: add component to change the location

--- src/components/MapComponent/MapComponent.tsx
+++ src/components/MapComponent/MapComponent.tsx
@@ -1,12 +1,22 @@
 import { InformationContainer } from "../InformationContainer/InformationContainer";
 import "./MapComponent.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 type Props = {
   center: [number, number];
 };
 
-export const MapComponent: React.FC<Props> = ({ center }) => {
+type LocationProps = {
+  center: [number, number];
+  zoom: number;
+};
+
+export const MapComponent = ({ center }: Props) => {
+  function ChangeLocation({ center, zoom }: LocationProps) {
+    const map = useMap();
+    map.flyTo(center);
+    return null;
+  }
   return (
     <MapContainer
       className="map"
@@ -18,6 +28,7 @@ export const MapComponent: React.FC<Props> = ({ center }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <ChangeLocation center={center} zoom={12} />
       <Marker position={center}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
