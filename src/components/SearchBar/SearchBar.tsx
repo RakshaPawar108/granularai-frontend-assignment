5f2ed51c51dc3558e950e124327ef5973f39c7ff
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-01-27 12:35:34 +0530

    style: add styling to header component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HistoryButton } from "../HistoryButton/HistoryButton";
 import "./SearchBar.css";
 import { SearchData } from "../../data/searchdata";
+import { Header } from "../Header/Header";
 
 type Props = {
   searchQuery: string;
@@ -21,33 +22,36 @@ export const SearchBar = ({
   setHistoryList,
 }: Props) => {
   return (
-    <div className="searchbar-container">
-      <input
-        type="text"
-        placeholder="Search"
-        className="searchbar"
-        value={searchQuery}
-        onChange={(e) => handleStringChange(e.target.value)}
-      />
-      <div
-        className="search-button"
-        onClick={() => {
-          setChangeLocation(true);
-          if (newData) {
-            setHistoryList((historyList: string[]) => [
-              ...new Set([
-                ...historyList,
-                newData.namedetails["name:en"] ??
-                  newData.namedetails.name ??
-                  newData.display_name,
-              ]),
-            ]);
-          }
-        }}
-      >
-        Fly To Location
+    <div className='main-wrapper'>
+      <Header />
+      <div className="searchbar-container">
+        <input
+          type="text"
+          placeholder="Search"
+          className="searchbar"
+          value={searchQuery}
+          onChange={(e) => handleStringChange(e.target.value)}
+        />
+        <div
+          className="search-button"
+          onClick={() => {
+            setChangeLocation(true);
+            if (newData) {
+              setHistoryList((historyList: string[]) => [
+                ...new Set([
+                  ...historyList,
+                  newData.namedetails["name:en"] ??
+                    newData.namedetails.name ??
+                    newData.display_name,
+                ]),
+              ]);
+            }
+          }}
+        >
+          Fly To Location
+        </div>
+        <HistoryButton historyClickHandler={historyClickHandler} />
       </div>
-      <HistoryButton historyClickHandler={historyClickHandler} />
     </div>
   );
 };
